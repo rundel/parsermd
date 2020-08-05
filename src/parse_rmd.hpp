96e53371aba40879e839d58a4d5523c4be9dc6ad
Colin Rundel <rundel@gmail.com>
2020-08-05 16:14:37 +0100

    Group consecutive text lines

--- src/parse_rmd.hpp
+++ src/parse_rmd.hpp
@@ -20,7 +20,7 @@ namespace client { namespace ast {
     int level;
   };
 
-  struct line : x3::variant<chunk, heading, std::string> {
+  struct line : x3::variant<chunk, heading, std::vector<std::string>> {
     using base_type::base_type;
     using base_type::operator=;
   };
@@ -52,7 +52,6 @@ namespace Rcpp {
 #include <Rcpp.h>
 
 namespace Rcpp {
-
   template <> SEXP wrap(client::ast::heading const& h) {
     Rcpp::List res = Rcpp::List::create(
       Rcpp::Named("name")  = h.name,
@@ -67,7 +66,7 @@ namespace Rcpp {
     struct line_visitor {
       SEXP operator()(client::ast::chunk const& c) { return Rcpp::wrap(c); }
       SEXP operator()(client::ast::heading const& h) { return Rcpp::wrap(h); }
-      SEXP operator()(std::string const& s) {return Rcpp::wrap(s); }
+      SEXP operator()(std::vector<std::string> const& s) {return Rcpp::wrap(s); }
     } v;
 
     return boost::apply_visitor(v, line);
@@ -94,11 +93,16 @@ namespace Rcpp {
 namespace client { namespace parser {
   namespace x3 = boost::spirit::x3;
 
-  auto const text = x3::rule<struct _, std::string>{"line"}
+
+
+  auto const text_line = x3::rule<struct _, std::string>{"text line"}
   = x3::raw[ x3::lexeme[
       !(x3::lit("```{") | x3::lit("---") | x3::lit("#")) >>
       *(x3::char_ - x3::eol)
     ] ];
+
+  auto const text = x3::rule<struct _, std::vector<std::string>>{"text"}
+  = +(text_line >> x3::eol);
 
   static auto headn = [](int n) {
     return (
@@ -112,10 +116,10 @@ namespace client { namespace parser {
   };
 
   auto const heading = x3::rule<struct _, client::ast::heading>{"heading"}
-  = headn(6) | headn(5) | headn(4) | headn(3) | headn(2) | headn(1);
+  = (headn(6) | headn(5) | headn(4) | headn(3) | headn(2) | headn(1)) >> x3::eol;
 
   auto const entry = x3::rule<struct _, client::ast::line>{"line"}
-  = chunk | (heading >> x3::eol) | (text >> x3::eol);
+  = chunk | heading | text;
 
   auto const rmd = x3::rule<struct _, client::ast::rmd>{"rmd"}
   = -yaml >> *entry;
