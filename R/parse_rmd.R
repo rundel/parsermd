243c9e7005c28de8db7148e0e6ef24b1478eb26f
Colin Rundel <rundel@gmail.com>
2020-08-10 10:28:11 +0100

    print tree

--- R/parse_rmd.R
+++ R/parse_rmd.R
@@ -53,3 +53,63 @@ as_tibble.rmd_ast = function(ast, include_yaml = TRUE) {
     section
   )
 }
+
+as_tree = function(obj, ...) {
+  UseMethod("as_tree")
+}
+
+as_tree.default = function(obj, ...) {
+  stop("Unsupports type:", class(obj))
+}
+
+as_tree.rmd_ast = function(ast, root = "Rmd AST") {
+
+  tree = list()
+  tree[[root]] = character()
+
+  sections = root
+  level = 0
+
+  for(j in seq_along(ast)) {
+    x = ast[[j]]
+
+    if (inherits(x, "rmd_yaml")) {
+      text = "YAML"
+      label = paste0("[", length(x)," lines]")
+    } else if (inherits(x, "rmd_heading")) {
+      text = paste("Heading")
+      label = paste0("[h", x$level, "] - ",  cli::style_bold(x$name))
+
+      sub = level < x$level
+      level = level[sub]
+      sections = sections[sub]
+    } else if (inherits(x, "rmd_chunk")) {
+      text = paste("Chunk")
+      name = if (x$name != "") cli::style_bold(x$name)
+             else cli::style_italic("<unnamed>")
+      opt = if (length(x$options) == 1) "1 opt, "
+            else if (length(x$options) != 0) paste0(length(x$options), " opts, ")
+            else ""
+      label = paste0("[", x$engine, ", ", opt, length(x$code), " lines] - ", name)
+    } else if (inherits(x, "character")) {
+      text = "Markdown"
+      label = paste0("[", length(x), " lines]")
+    } else {
+      stop("Unexpected type in AST")
+    }
+
+    cur_sec = sections[length(sections)]
+    node = paste(text, cli::col_grey(label))
+    tree[[cur_sec]] = append(tree[[cur_sec]], node)
+    tree[[node]] = character()
+
+    if (inherits(x, "rmd_heading")) {
+      sections = append(sections, node)
+      level = append(level, x$level)
+    }
+  }
+
+  d = data.frame(name = names(tree), children = I(unname(tree)))
+
+  cli::tree(d)
+}
