b5d97efa601da33a1a22dbacd20088f78db1421d
Colin Rundel <rundel@gmail.com>
2020-08-10 12:06:24 +0100

    Fix printing

--- R/ast_print.R
+++ R/ast_print.R
@@ -5,12 +5,17 @@ trim_blank_lines = function(x) {
 }
 
 #' @export
-print.rmd_ast = function(ast) {
-  for(i in seq_along(ast)) {
-    if (class(ast[[i]]) == "character")
-      cat(ast[[i]], sep="\n")
-    else
-      print(ast[[i]])
+print.rmd_ast = function(ast, as_tree = TRUE) {
+
+  if (as_tree) {
+    print(as_tree(ast))
+  } else {
+    for(i in seq_along(ast)) {
+      if (class(ast[[i]]) == "character")
+        cat(ast[[i]], sep="\n")
+      else
+        print(ast[[i]])
+    }
   }
 }
 
@@ -38,3 +43,66 @@ print.rmd_heading = function(h) {
 print.rmd_yaml = function(yaml) {
   cli::boxx(cli::col_silver(yaml), padding=0)
 }
+
+
+
+as_tree = function(obj, ...) {
+  UseMethod("as_tree")
+}
+
+as_tree.default = function(obj, ...) {
+  stop("Unsupports type:", class(obj))
+}
+
+as_tree.rmd_ast = function(ast, root = "Rmd AST") {
+
+  tree = list()
+  tree[[root]] = character()
+
+  sections = root
+  level = 0
+
+  for(j in seq_along(ast)) {
+    x = ast[[j]]
+
+    if (inherits(x, "rmd_yaml")) {
+      text = "YAML"
+      label = paste0("[", length(x)," lines]")
+    } else if (inherits(x, "rmd_heading")) {
+      text = paste("Heading")
+      label = paste0("[h", x$level, "] - ",  cli::style_bold(x$name))
+
+      sub = level < x$level
+      level = level[sub]
+      sections = sections[sub]
+    } else if (inherits(x, "rmd_chunk")) {
+      text = paste("Chunk")
+      name = if (x$name != "") cli::style_bold(x$name)
+      else cli::style_italic("<unnamed>")
+      opt = if (length(x$options) == 1) "1 opt, "
+      else if (length(x$options) != 0) paste0(length(x$options), " opts, ")
+      else ""
+      label = paste0("[", x$engine, ", ", opt, length(x$code), " lines] - ", name)
+    } else if (inherits(x, "character")) {
+      text = "Markdown"
+      label = paste0("[", length(x), " lines]")
+    } else {
+      stop("Unexpected type in AST")
+    }
+
+    cur_sec = sections[length(sections)]
+    node = paste(text, cli::col_grey(label))
+    tree[[cur_sec]] = append(tree[[cur_sec]], node)
+    tree[[node]] = character()
+
+    if (inherits(x, "rmd_heading")) {
+      sections = append(sections, node)
+      level = append(level, x$level)
+    }
+  }
+
+  d = data.frame(name = names(tree), children = I(unname(tree)))
+
+  cli::tree(d)
+}
+
