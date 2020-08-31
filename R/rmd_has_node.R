3e251d421e0cfd96d3ec1d817e62c88f01124d51
Colin Rundel <rundel@gmail.com>
2020-08-31 15:46:57 +0100

    rmd_has_node, rmd_has_chunk, and rmd_has_markdown with tests

--- R/rmd_has_node.R
+++ R/rmd_has_node.R
@@ -1,5 +1,5 @@
 #' @export
-rmd_has_node = function(obj, ...) {
+rmd_has_node = function(obj, sec_refs = NULL, type_refs = NULL, name_refs = NULL) {
   UseMethod("rmd_has_node")
 }
 
@@ -10,7 +10,7 @@ rmd_has_node.default = function(obj, ...) {
 
 #' @export
 rmd_has_node.rmd_ast = function(ast, sec_refs = NULL, type_refs = NULL, name_refs = NULL) {
-  subset = comb_subset(ast, sec_refs, type_refs, name_refs, combine = `|`)
+  subset = comb_subset(ast, sec_refs, type_refs, name_refs, combine = `|`, inc_parents = FALSE)
   any(subset)
 }
 
@@ -20,3 +20,13 @@ rmd_has_node.rmd_tibble = function(df, sec_refs = NULL, type_refs = NULL, name_r
 }
 
 
+#' @export
+rmd_has_chunk = function(obj, sec_refs = NULL, name_refs = NULL) {
+  rmd_has_node(obj, sec_refs = sec_refs, type_refs = "rmd_chunk", name_refs = name_refs)
+}
+
+
+#' @export
+rmd_has_markdown = function(obj, sec_refs = NULL) {
+  rmd_has_node(obj, sec_refs = sec_refs, type_refs = "rmd_markdown")
+}
