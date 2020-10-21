b151505c3186a5189433fe79c7f5a0b17cc8609c
Colin Rundel <rundel@gmail.com>
2020-10-21 15:32:13 +0100

    Maybe a working chunk parser with decent errors

--- src/parse_chunk.hpp
+++ src/parse_chunk.hpp
@@ -16,24 +16,9 @@
 namespace client { namespace parser {
   namespace x3 = boost::spirit::x3;
 
-  auto const engine = x3::rule<struct _, std::string> {"engine"}
-    = x3::lexeme[ +x3::char_("A-Za-z0-9_") ];
-
-  // This spec is based on Sec 3.2 of the Sweave manual
-  auto const label = x3::rule<struct _, std::string>{"chunk label"}
-    = x3::lexeme[ x3::raw[ +x3::char_("A-Za-z0-9#+_-") ] ];
-
-  auto const details = x3::rule<struct _, client::ast::details>{"details"}
-    = (  (label >> ','            >> (option % ','))
-       | (x3::attr(std::string()) >> -x3::lit(',') >> (option % ','))
-       | (label >> -x3::lit(',')  >> x3::attr(std::vector<ast::option>()))
-       | (x3::attr(std::string()) >> x3::attr(std::vector<ast::option>()))
-      );
-
-
+  // Indent stuff
 
   // indent code based on https://stackoverflow.com/questions/62143841/spirit-x3-referring-to-a-previously-matched-value
-
   auto const indent_pat = x3::char_(" \t>");
 
   struct indent{};
@@ -45,44 +30,85 @@ namespace client { namespace parser {
     = (*indent_pat)
       [([](auto& ctx) { _pass(ctx) = (x3::get<indent>(ctx) == _attr(ctx)); })];
 
+  auto check_indent = [](auto& ctx) {
+    size_t n = x3::get<indent>(ctx).length();
+    _pass(ctx) = (x3::get<indent>(ctx) ==  _attr(ctx).substr(0, n)); // Compare the indents
+    _attr(ctx).erase(0, n);                                          // erase the first n chars
+    // so code line wont have indent
+    _val(ctx) = _attr(ctx);
+  };
 
-  auto const code = x3::rule<struct _, std::string, true> {"code"}
-    = (x3::raw[
-        !(*indent_pat >> x3::lit("```")) >>
-        *(x3::char_ - x3::eol)
-      ])
-      [([](auto& ctx) {
-        size_t n = x3::get<indent>(ctx).length();
-        _pass(ctx) = (x3::get<indent>(ctx) ==  _attr(ctx).substr(0, n)); // Compare the indents
-        _attr(ctx).erase(0, n);                                          // erase the first n chars
-                                                                         // so code line wont have indent
-        _val(ctx) = _attr(ctx);
-      })];
-  ;
+  // Code stuff
+
+  auto const code_line = x3::rule<struct _, std::string, true> {"Chunk code line"}
+  = (x3::raw[
+      !(*indent_pat >> x3::lit("```")) >>
+      *(x3::char_ - x3::eol)
+    ]) [check_indent];
+
+  auto const chunk_code = x3::rule<struct _, std::vector<std::string>> {"Chunk code"}
+  = x3::lexeme[ *(code_line >> x3::eol) ];
+
+
+  // Chunk arg stuff
+
+  auto const engine = x3::rule<struct _, std::string> {"chunk engine"}
+  = (x3::lexeme[ +x3::char_("A-Za-z0-9_") ]) [
+    ([](auto& ctx){ _val(ctx) = _attr(ctx); }) //Override the default concat behavior
+  ];
+
+  auto const label = x3::rule<struct _, std::string> {"chunk label"}
+  = x3::lexeme[ +x3::char_("A-Za-z0-9#+_-") ]; // Based on Sec 3.2 of the Sweave manual
+
+
+
+  // Chunk stuff
+
+  auto const chunk_template = x3::rule<struct _> {"chunk template"}
+  = (x3::lit("```{") >> *(~x3::char_("}\n"))) > x3::lit('}') > x3::eol >> // Start
+    *(!x3::lit("```") >> *(x3::char_- x3::eol) >> x3::eol) >              // code
+    x3::lit("```");                                                       // end
+
+
+  auto chunk_start_wrap = [](auto p) {
+    return (
+      x3::lexeme[ start_indent >> x3::lit("```{") ] >>
+      x3::skip(x3::blank)[
+        x3::expect[engine] >>
+        -x3::lit(",") >>
+        p >>
+        x3::lit("}") >>
+        x3::eol
+      ]
+    );
+  };
+
+  auto const chunk_start = x3::rule<struct _, client::ast::chunk_args> {"chunk start"}
+  = (  chunk_start_wrap( x3::attr(std::string()) >> x3::attr(std::vector<ast::option>()) )
+     | chunk_start_wrap( label >> -x3::lit(',') >> x3::attr(std::vector<ast::option>()) )
+     | chunk_start_wrap( x3::attr(std::string()) >> (option % ',') )
+     | chunk_start_wrap( label > x3::lit(",") > (option % ',') )
+    );
+
+
+  auto const chunk_end = x3::rule<struct _> {"chunk end"}
+  = (  x3::lexeme[ x3::omit[ end_indent ] >> x3::lit("```") ] >> *x3::lit(" ") >> x3::eol
+    | &x3::lexeme[ x3::omit[ end_indent ] >> x3::lit("```{") ] );
+  // Chunk can be ended by a new chunk starting see Yihui's book Sec 5.1.4
+  // look ahead to match but not consume this new chunk
 
   struct chunk_class : error_handler, x3::annotate_on_success {};
-
   x3::rule<chunk_class, client::ast::chunk> const chunk = "chunk";
 
-  auto const chunk_def //= x3::rule<struct _, client::ast::chunk, true>{"chunk"}
+  auto const chunk_def
     = x3::with<indent>(std::string()) [
-        // Chunk start
-        x3::lexeme[ start_indent >> (x3::lit("```") > x3::lit("{")) ] >
-        x3::skip(x3::blank)[
-          engine >> -x3::lit(",") >> details
-        ] >
-        (x3::lit("}") >> x3::skip(x3::blank)[x3::eol]) >>
-        // Chunk code
-        x3::lexeme[ *(code >> x3::eol) ] >
-        // Chunk end
-        (    x3::lexeme[ x3::omit[ end_indent ] >> x3::lit("```") ] >> *x3::lit(" ") >> x3::eol
-          | &x3::lexeme[ x3::omit[ end_indent ] >> x3::lit("```{") ] )
-            // Chunk can be ended by a new chunk starting see Yihui's book Sec 5.1.4
-            // nasdasdasdeed to match but not consume this new chunk
+        &chunk_template >> // look ahead check of chunk structure
+        chunk_start >>
+        chunk_code >>
+        chunk_end
       ];
+
   BOOST_SPIRIT_DEFINE(chunk);
-
-
 
 } }
 
--- src/parse_chunk_ast.hpp
+++ src/parse_chunk_ast.hpp
@@ -11,26 +11,26 @@
 namespace client { namespace ast {
   namespace x3 = boost::spirit::x3;
 
-  struct details : x3::position_tagged {
+  struct chunk_args : x3::position_tagged {
+    std::string indent;
+    std::string engine;
     std::string name;
     std::vector<option> options;
   };
 
   struct chunk : x3::position_tagged{
-    std::string indent;
-    std::string engine;
-    details d;
+    chunk_args args;
     std::vector<std::string> code;
   };
 } }
 
 BOOST_FUSION_ADAPT_STRUCT(
-  client::ast::details,
-  name, options
+  client::ast::chunk_args,
+  indent, engine, name, options
 )
 
 BOOST_FUSION_ADAPT_STRUCT(
   client::ast::chunk,
-  indent, engine, d, code
+  args, code
 )
 #endif
