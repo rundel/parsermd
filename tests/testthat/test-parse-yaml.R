97e8f1bc4fc2cf5a6d596eec2f11544c76331e07
Colin Rundel <rundel@gmail.com>
2020-10-21 18:51:41 +0100

    Fix yaml tests to use the new output format

--- tests/testthat/test-parse-yaml.R
+++ tests/testthat/test-parse-yaml.R
@@ -1,14 +1,18 @@
+as_yaml = function(...) {
+  structure(
+    c(...), class = "rmd_yaml"
+  )
+}
+
 test_that("yaml parsing - good yaml", {
-
   expect_equal(parsermd:::check_yaml_parser("---\n---\n"),
-               character())
+               as_yaml(character()))
   expect_equal(parsermd:::check_yaml_parser("---\nvalue: 1\n---\n"),
-               "value: 1")
+               as_yaml("value: 1"))
   expect_equal(parsermd:::check_yaml_parser("---\nvalue: 1\nname: bob\n---\n"),
-               c("value: 1", "name: bob"))
-
+               as_yaml("value: 1", "name: bob"))
   expect_equal(parsermd:::check_yaml_parser("---\nvalue: \"---\"\n---\n"),
-               c("value: \"---\""))
+               as_yaml("value: \"---\""))
 })
 
 test_that("yaml parsing - bad yaml", {
