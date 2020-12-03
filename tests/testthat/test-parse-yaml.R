2e1a597406a987e10694dcb9f9f0ea91636583eb
Colin Rundel <rundel@gmail.com>
2020-12-03 11:46:34 +0000

    Indented text is a problem here - remove

--- tests/testthat/test-parse-yaml.R
+++ tests/testthat/test-parse-yaml.R
@@ -5,25 +5,35 @@ as_yaml = function(...) {
 }
 
 test_that("yaml parsing - good yaml", {
-  expect_equal(parsermd:::check_yaml_parser("---\n---\n"),
-               as_yaml(character()))
-  expect_equal(parsermd:::check_yaml_parser("---\nvalue: 1\n---\n"),
-               as_yaml("value: 1"))
-  expect_equal(parsermd:::check_yaml_parser("---\nvalue: 1\nname: bob\n---\n"),
-               as_yaml("value: 1", "name: bob"))
-  expect_equal(parsermd:::check_yaml_parser("---\nvalue: \"---\"\n---\n"),
-               as_yaml("value: \"---\""))
+  expect_equal(
+    parsermd:::check_yaml_parser("---\n---\n"),
+    as_yaml(character())
+  )
+
+  expect_equal(
+    parsermd:::check_yaml_parser("---\nvalue: 1\n---\n"),
+    as_yaml("value: 1")
+  )
+
+  expect_equal(
+    parsermd:::check_yaml_parser("---\nvalue: 1\nname: bob\n---\n"),
+    as_yaml("value: 1", "name: bob")
+  )
+
+  expect_equal(
+    parsermd:::check_yaml_parser("---\nvalue: \"---\"\n---\n"),
+    as_yaml("value: \"---\"")
+  )
 })
 
 test_that("yaml parsing - authors", {
 
   yaml = '---
-    title: "Title"
-    author:
-    - John Doe
-    - Jane Doe
-  ---
-  '
+title: "Title"
+author:
+- John Doe
+- Jane Doe
+---\n'
 
   yaml_res = strsplit(yaml, "\n")[[1]]
   yaml_res = as_yaml( yaml_res[-c(1, length(yaml_res))] )
