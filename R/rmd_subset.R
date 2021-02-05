1e039e1d8ac0a998383b2421a5542f21059e4d54
Colin Rundel <rundel@gmail.com>
2021-02-05 22:24:15 +0000

    Add docs and fix S3 arg consistency

--- R/rmd_subset.R
+++ R/rmd_subset.R
@@ -1,16 +1,28 @@
+#' @title Subset the nodes of an rmd object
+#' @description
+#' Subset an rmd object based on sections, node types, or names.
+#'
+#' @param sec_refs Section references, TODO - add details
+#' @param type_refs Node type references, TODO - add details
+#' @param name_refs Name references, TODO - add details
+#' @param exclude Should the matching nodes be excluded
+#' @param keep_yaml Should the document yaml be kept.
+#' @param keep_yaml Should the document setup chunk be kept.
+#' @param ... Unused, for extensibility.
+#'
 #' @export
-rmd_subset = function(obj, sec_refs = NULL, type_refs = NULL, name_refs = NULL, exclude = FALSE, keep_yaml = TRUE, keep_setup = FALSE) {
+rmd_subset = function(x, sec_refs = NULL, type_refs = NULL, name_refs = NULL, exclude = FALSE, keep_yaml = TRUE, keep_setup = FALSE, ...) {
   UseMethod("rmd_subset")
 }
 
 #' @export
-rmd_subset.default = function(obj, ...) {
-  stop("Unable to subset an object with class: ", class(obj))
+rmd_subset.default = function(x, ...) {
+  stop("Unable to subset an object with class: ", class(x))
 }
 
 #' @export
-rmd_subset.rmd_ast = function(ast, sec_refs = NULL, type_refs = NULL, name_refs = NULL, exclude = FALSE, keep_yaml = TRUE, keep_setup = FALSE) {
-  subset = comb_subset(ast, sec_refs, type_refs, name_refs, combine = `|`, inc_parents = !exclude)
+rmd_subset.rmd_ast = function(x, sec_refs = NULL, type_refs = NULL, name_refs = NULL, exclude = FALSE, keep_yaml = TRUE, keep_setup = FALSE, ...) {
+  subset = comb_subset(x, sec_refs, type_refs, name_refs, combine = `|`, inc_parents = !exclude)
 
   if (exclude)
     subset = !subset
@@ -24,44 +36,43 @@ rmd_subset.rmd_ast = function(ast, sec_refs = NULL, type_refs = NULL, name_refs 
     subset = subset | (rmd_node_type(ast) %in% c("rmd_yaml", "rmd_yaml_list"))
   }
 
+  x = x[subset]
+  class(x) = c("rmd_ast", "list")
 
-  ast = ast[subset]
-  class(ast) = c("rmd_ast", "list")
-
-  ast
+  x
 }
 
 #' @export
-rmd_subset.rmd_tibble = function(df, sec_refs = NULL, type_refs = NULL, name_refs = NULL, exclude = FALSE, keep_yaml = TRUE, keep_setup = FALSE) {
-  sec_cols = names(df)[grepl("^sec_h", names(df))]
+rmd_subset.rmd_tibble = function(x, sec_refs = NULL, type_refs = NULL, name_refs = NULL, exclude = FALSE, keep_yaml = TRUE, keep_setup = FALSE, ...) {
+  sec_cols = names(x)[grepl("^sec_h", names(x))]
   bad_cols = sec_cols[!sec_cols %in% paste0("sec_h", 1:6)]
 
   if (length(bad_cols) != 0)
       stop("The following columns must be renamed: ", bad_cols, call. = FALSE)
 
-  subset = comb_subset(df$ast, sec_refs, type_refs, name_refs, combine = `|`, inc_parents = !exclude)
+  subset = comb_subset(x$ast, sec_refs, type_refs, name_refs, combine = `|`, inc_parents = !exclude)
 
   if (exclude)
     subset = !subset
 
   if (keep_setup) {
-    labels = rmd_node_label(df$ast)
+    labels = rmd_node_label(x$ast)
     subset = subset | (labels == "setup" & !is.na(labels))
   }
 
   if (keep_yaml) {
-    subset = subset | (rmd_node_type(df$ast) %in% c("rmd_yaml", "rmd_yaml_list"))
+    subset = subset | (rmd_node_type(x$ast) %in% c("rmd_yaml", "rmd_yaml_list"))
   }
 
-  df = df[subset,]
+  x = x[subset,]
 
-  df = dplyr:: bind_cols(
-    dplyr::bind_rows(rmd_node_sections(df$ast)),  # add new sec_h* columns
-    dplyr::select(df, -dplyr::starts_with("sec_h")) # drop old sec_h* columns
+  x = dplyr:: bind_cols(
+    dplyr::bind_rows(rmd_node_sections(x$ast)),  # add new sec_h* columns
+    dplyr::select(x, -dplyr::starts_with("sec_h")) # drop old sec_h* columns
   )
-  class(df) = c("rmd_tibble", class(df))
+  class(x) = c("rmd_tibble", class(x))
 
-  df
+  x
 }
 
 
