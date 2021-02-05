78ced6c1e48a6ee22d5d38459e878a63d34db480
Colin Rundel <rundel@gmail.com>
2021-02-05 17:38:01 +0000

    as_ast documentation

--- R/as_ast.R
+++ R/as_ast.R
@@ -1,14 +1,28 @@
+#' @title Convert an object into an `rmd_ast`.
+#'
+#' @description
+#' Currently only supports conversion of `rmd_tibble`s.
+#'
+#' @param x Object to convert
+#'
+#' @examples
+#' parse_rmd(system.file("hw01.Rmd", package="parsermd")) %>%
+#'   as_tibble() %>%
+#'   as_ast()
+#'
 #' @export
-as_ast = function(obj, ...) {
+as_ast = function(x, ...) {
   UseMethod("as_ast")
 }
 
+#' @rdname as_ast
 #' @export
-as_ast.default = function(obj, ...) {
-  stop("Unsupports type: ", class(obj))
+as_ast.default = function(x, ...) {
+  stop("Unsupported class: ", paste0(class(x), collapse = ", "))
 }
 
+#' @rdname as_ast
 #' @export
-as_ast.rmd_tibble = function(df) {
-  df$ast
+as_ast.rmd_tibble = function(x, ...) {
+  x$ast
 }
