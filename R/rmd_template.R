965d32c3fdabdc4def617a1d7d2f0293be274d2b
Colin Rundel <rundel@gmail.com>
2021-02-05 20:17:03 +0000

    Add docs

--- R/rmd_template.R
+++ R/rmd_template.R
@@ -1,22 +1,46 @@
+#' @title Create a template from an `rmd` object.
+#' @description
+#' Templates are objects which are meant to capture the structure of an RMarkdown document and facilitate the
+#' comparison between the template and new Rmd documents, usually to ensure the structure and/or content matches
+#' sufficiently.
+#'
+#' @param rmd RMarkdown document in the form of an `rmd_ast` or `rmd_tibble`.
+#' @param keep_content Should the template keep the document's content (markdown text and chunk code).
+#' @param keep_labels Should the template keep the document's code chunk labels.
+#' @param keep_headings Should the template keep the document's headings.
+#' @param keep_yaml Should the template keep the document's yaml.
+#'
+#' @examples
+#' rmd = parse_rmd(system.file("hw01.Rmd", package="parsermd"))
+#'
+#' rmd_subset(rmd, sec_refs = c("Exercise *", "Solution")) %>%
+#'   rmd_template()
+#'
 #' @export
-rmd_template = function(rmd, keep_content = FALSE, keep_labels = TRUE, keep_headings = FALSE, keep_yaml = FALSE) {
+rmd_template = function(rmd, keep_content = FALSE, keep_labels = TRUE, keep_headings = FALSE, keep_yaml = FALSE, ...) {
   UseMethod("rmd_template")
 }
 
-#' @export
+#' @exportS3Method
 rmd_template.default = function(rmd, ...) {
-  stop("Unable to generate an Rmd template from an object with class: ", class(rmd))
+  stop("Unable to generate an Rmd template from an object with class: ", paste(class(rmd), collapse=", "))
 }
 
 
-#' @export
-rmd_template.rmd_ast = function(rmd, keep_content = FALSE, keep_labels = TRUE, keep_headings = FALSE, keep_yaml = FALSE) {
-  rmd_template( as_tibble(rmd), keep_content = keep_content, keep_labels = keep_labels )
+#' @exportS3Method
+rmd_template.rmd_ast = function(rmd, keep_content = FALSE, keep_labels = TRUE, keep_headings = FALSE, keep_yaml = FALSE, ...) {
+  rmd_template(
+    as_tibble(rmd),
+    keep_content = keep_content,
+    keep_labels = keep_labels,
+    keep_headings = keep_headings,
+    keep_yaml = keep_yaml
+  )
 }
 
 
-#' @export
-rmd_template.rmd_tibble = function(rmd, keep_content = FALSE, keep_labels = TRUE, keep_headings = FALSE, keep_yaml = FALSE) {
+#' @exportS3Method
+rmd_template.rmd_tibble = function(rmd, keep_content = FALSE, keep_labels = TRUE, keep_headings = FALSE, keep_yaml = FALSE, ...) {
   if (!keep_labels)
     rmd = dplyr::select(rmd, -.data[["label"]])
 
