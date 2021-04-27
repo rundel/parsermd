02a627b78d80d8f42089947c5ffc56afad5dd9a7
Colin Rundel <rundel@gmail.com>
2021-04-27 12:38:44 +0100

    Fixes

---
+++ tests/testthat/setup.R
@@ -0,0 +1,23 @@
+
+# From https://github.com/r-lib/cli/issues/226
+
+local_cli_config = function(
+  unicode = FALSE, dynamic = FALSE, ansi = FALSE,
+  num_colors = 1, .local_envir = parent.frame()
+) {
+  withr::local_options(
+    cli.dynamic = dynamic,
+    cli.ansi = ansi,
+    cli.unicode = unicode,
+    crayon.enabled = num_colors > 1,
+    crayon.colors = num_colors,
+    .local_envir = .local_envir
+  )
+  withr::local_envvar(
+    PKG_OMIT_TIMES = "true",
+    PKG_OMIT_SIZES = "true",
+    .local_envir = .local_envir
+  )
+}
+
+local_cli_config(unicode = TRUE, num_colors = 256)
