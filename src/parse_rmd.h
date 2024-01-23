21055773058e7370b62c12841f503e4ec865ecf9
Colin Rundel <rundel@gmail.com>
2024-01-23 15:32:16 -0500

    Count fdivs and throw errors if not balanced

--- src/parse_rmd.h
+++ src/parse_rmd.h
@@ -16,15 +16,39 @@
 namespace client { namespace parser {
   namespace x3 = boost::spirit::x3;
 
+  // fdiv counting
+
+  static int n_fdiv_open;
+  struct _n_fdiv_open{};
+
+  auto init_fdiv = [](auto& ctx) {
+    x3::get<_n_fdiv_open>(ctx).get() = 0;
+  };
+  auto open_fdiv = [](auto& ctx) {
+    x3::get<_n_fdiv_open>(ctx).get()++;
+  };
+  auto close_fdiv = [](auto& ctx) {
+    if (x3::get<_n_fdiv_open>(ctx).get() == 0) {
+      x3::_pass(ctx) = false;
+    } else {
+      x3::get<_n_fdiv_open>(ctx).get()--;
+    }
+  };
+  auto end_fdiv = [](auto& ctx) {
+    if (x3::get<_n_fdiv_open>(ctx).get() != 0) {
+      x3::_pass(ctx) = false;
+    }
+  };
+
   // Markdown text stuff
   auto const chunk_start_sig = (*indent_pat >> x3::lit("```{"));
   auto const heading_start_sig = x3::lit("#");
   auto const fdiv_start_sig = x3::lit(":::");
-  auto const yaml_start_sig = x3::lit("---");
+  //auto const yaml_start_sig = x3::lit("---");   // Don't need to check since this is valid md
   auto const shortcode_start_sig = x3::lit("{{<");
 
   auto const invalid_start = x3::rule<struct _>{"invalid start"}
-  = chunk_start_sig | heading_start_sig | fdiv_start_sig | yaml_start_sig | shortcode_start_sig;
+  = chunk_start_sig | heading_start_sig | fdiv_start_sig | shortcode_start_sig;
 
   auto const text_line = x3::rule<struct _, std::string>{"markdown text line"}
   = !invalid_start >> x3::lexeme[ *(x3::char_ - x3::eol) ];
@@ -32,12 +56,20 @@ namespace client { namespace parser {
   auto const text = x3::rule<struct _, std::vector<std::string>>{"markdown text"}
   = +(text_line >> x3::eol);
 
+
   // Rmd stuff
   auto const element = x3::rule<struct _, client::ast::element> {"rmd element"}
-  = (chunk | heading | yaml | shortcode | fdiv_close | fdiv_open | text) >> *x3::eol;
+  %= x3::with<_n_fdiv_open>(std::ref(n_fdiv_open))[
+    (chunk | heading | yaml | shortcode | fdiv_close[close_fdiv] | fdiv_open[open_fdiv] | text) >> *x3::eol
+  ];
 
   auto const rmd = x3::rule<struct _, client::ast::rmd> {"rmd"}
-  = *element;
+  %= x3::with<_n_fdiv_open>(std::ref(n_fdiv_open))[
+     x3::eps[init_fdiv] >>
+     *element >>
+     x3::eps[end_fdiv]
+  ];
+
 } }
 
 #endif
