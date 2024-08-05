36656cd2c421ab8b53a4503bede895f1bfe3fa68
Colin Rundel <rundel@gmail.com>
2024-08-05 14:31:02 -0700

    Revised implementation of collections -> ast

--- R/as_ast.R
+++ R/as_ast.R
@@ -53,25 +53,56 @@ fix_heading_level = function(ast, name) {
 }
 
 #' @exportS3Method
-as_ast.rmd_collection = function(x, ...) {
-  purrr::map2(
-    x[["name"]], x[["ast"]],
-    function(name, ast) {
-      c(
-        list(create_heading(name, 1)),
-        rmd_select(ast, !has_type(c("rmd_yaml_list", "rmd_yaml"))) %>%
-          fix_heading_level(name)
-      ) %>%
-        magrittr::set_class("rmd_ast")
-    }
-  ) %>%
-    unlist(recursive = FALSE) %>%
-    magrittr::set_class("rmd_ast")
+as_ast.rmd_collection = function(
+    x, ..., strip_yaml = TRUE, filename_headings = TRUE,
+    adjust_headings = TRUE, front_matter = NULL, collapse = NULL
+) {
+
+  if (strip_yaml)
+    x = rmd_select(x, !has_type(c("rmd_yaml_list", "rmd_yaml")))
+
+  if (adjust_headings) {
+    x[["ast"]] = purrr::map2(
+      x[["ast"]], x[["name"]],
+      fix_heading_level
+    )
+  }
+
+  if (filename_headings) {
+    x[["ast"]] = purrr::map2(
+      x[["ast"]], x[["name"]],
+      function(ast, name) {
+        c(list(create_heading(name, 1)), ast) %>%
+          magrittr::set_class("rmd_ast")
+      }
+    )
+  }
+
+  if (!is.null(collapse)) {
+    x[["ast"]] = c(
+      x[["ast"]][1],
+      purrr::map(
+        x[["ast"]][-1], function(ast) c(list(collapse), ast)
+      )
+    )
+  }
+
+  ast = unlist(x[["ast"]], recursive = FALSE)
+  if (!is.null(front_matter))
+    ast = c(list(front_matter), ast)
+
+  magrittr::set_class(ast, "rmd_ast")
 }
 
 #' @exportS3Method
-as_ast.qmd_collection = function(x, ...) {
-  as_ast.rmd_collection(x, ...)
+as_ast.qmd_collection = function(
+    x, ..., strip_yaml = TRUE, filename_headings = TRUE,
+    adjust_headings = TRUE, front_matter = NULL, collapse = NULL
+) {
+  as_ast.rmd_collection(
+    x, ..., strip_yaml = strip_yaml, filename_headings = filename_headings,
+    adjust_headings = adjust_headings, front_matter = front_matter, collapse = collapse
+  )
 }
 
 
