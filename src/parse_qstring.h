2831f99e4b4803cecaf87f4e7c37c79dab34e003
Colin Rundel <rundel@gmail.com>
2025-06-09 21:02:12 -0400

    Fix quoted string parsing

--- src/parse_qstring.h
+++ src/parse_qstring.h
@@ -7,10 +7,10 @@ namespace client { namespace parser {
 
   namespace x3 = boost::spirit::x3;
   auto const sq_string = x3::rule<struct _, std::string>{"single quoted string"}
-                       = x3::lexeme[ ( '\'' > *("\\\'" >> x3::char_('\'') | ~x3::char_('\'')) > '\'' ) ];
+                       = x3::lexeme[ '\'' >> *((x3::char_('\\') >> x3::char_("'")) | (x3::char_ - '\'')) >> '\'' ];
 
   auto const dq_string = x3::rule<struct _, std::string>{"double quoted string"}
-                       = x3::lexeme[ ( '"' > *("\\\"" >> x3::char_('"') | ~x3::char_('"')) > '"' ) ];
+                       = x3::lexeme[ '"' >> *((x3::char_('\\') >> x3::char_('"')) | (x3::char_ - '"')) >> '"' ];
 
   auto const bq_string = x3::rule<struct _, std::string>{"back quoted string"}
                        = x3::lexeme[ ( '`' > +(~x3::char_('`')) > '`' ) ];
