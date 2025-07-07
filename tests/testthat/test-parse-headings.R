321fc322336becc8eb8a8aa107bac8a16573e570
Colin Rundel <rundel@gmail.com>
2025-07-07 12:30:47 -0400

    Clean

--- tests/testthat/test-parse-headings.R
+++ tests/testthat/test-parse-headings.R
@@ -1,31 +1,31 @@
 test_that("markdown - headings", {
   expect_equal(
-    parsermd:::check_markdown_heading_parser("# h1\n"),
+    check_markdown_heading_parser("# h1\n"),
     rmd_heading("h1", 1L)
   )
 
   expect_equal(
-    parsermd:::check_markdown_heading_parser("## h2\n"),
+    check_markdown_heading_parser("## h2\n"),
     rmd_heading("h2", 2L)
   )
 
   expect_equal(
-    parsermd:::check_markdown_heading_parser("### h3\n"),
+    check_markdown_heading_parser("### h3\n"),
     rmd_heading("h3", 3L)
   )
 
   expect_equal(
-    parsermd:::check_markdown_heading_parser("#### h4\n"),
+    check_markdown_heading_parser("#### h4\n"),
     rmd_heading("h4", 4L)
   )
 
   expect_equal(
-    parsermd:::check_markdown_heading_parser("##### h5\n"),
+    check_markdown_heading_parser("##### h5\n"),
     rmd_heading("h5", 5L)
   )
 
   expect_equal(
-    parsermd:::check_markdown_heading_parser("###### h6\n"),
+    check_markdown_heading_parser("###### h6\n"),
     rmd_heading("h6", 6L)
   )
 })
@@ -33,22 +33,22 @@ test_that("markdown - headings", {
 test_that("markdown - headings - blank", {
 
   expect_equal(
-    parsermd:::check_markdown_heading_parser("# \n"),
+    check_markdown_heading_parser("# \n"),
     rmd_heading("", 1L)
   )
 
   expect_equal(
-    parsermd:::check_markdown_heading_parser("###### \n"),
+    check_markdown_heading_parser("###### \n"),
     rmd_heading("", 6L)
   )
 
   expect_equal(
-    parsermd:::check_markdown_heading_parser("#\n"),
+    check_markdown_heading_parser("#\n"),
     rmd_heading("", 1L)
   )
 
   expect_equal(
-    parsermd:::check_markdown_heading_parser("######\n"),
+    check_markdown_heading_parser("######\n"),
     rmd_heading("", 6L)
   )
 
